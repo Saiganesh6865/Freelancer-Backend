9c50732dafa8356a33dfa66f8d8c5ed01c23f4eb
Saiganesh6865 <135852674+Saiganesh6865@users.noreply.github.com>
2025-10-08 09:23:40 +0530

    Delete app directory

--- app/utils/response.py
+++
@@ -1,6 +0,0 @@
-# app/utils/response.py
-def success_response(data):
-    return {"success": True, "data": data}, 200
-
-def error_response(message, code=400):
-    return {"success": False, "error": message}, code
