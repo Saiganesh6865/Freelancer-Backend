ec165f64d60bef1874a5eadc0162d23199955861
Saiganesh6865 <135852674+Saiganesh6865@users.noreply.github.com>
2025-10-10 10:52:25 +0530

    Update auth_utils.py

--- app/auth/auth_utils.py
+++ app/auth/auth_utils.py
@@ -1,20 +1,19 @@
 from flask_jwt_extended import create_access_token, create_refresh_token
 from datetime import timedelta
 
-
 def generate_tokens(user):
     identity = str(user.id)
     claims = {"role": user.role}
-    
+
     access_token = create_access_token(
         identity=identity,
         additional_claims=claims,
-        expires_delta=timedelta(minutes=59)  # ⏰ Access token valid for 20 min
+        expires_delta=timedelta(minutes=60)  # Access token 60 min
     )
     refresh_token = create_refresh_token(
         identity=identity,
         additional_claims=claims,
-        expires_delta=timedelta(days=7)     # ⏳ Refresh token valid for 7 days
+        expires_delta=timedelta(days=7)      # Refresh token 7 days
     )
-    
+
     return access_token, refresh_token
