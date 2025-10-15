423d491b81263f2448b05513ffad363c97f5e145
Saiganesh6865 <135852674+Saiganesh6865@users.noreply.github.com>
2025-10-15 11:19:46 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(500), nullable=False)
